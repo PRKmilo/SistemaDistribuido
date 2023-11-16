1587d8d55cdb9b6b837560672dfa3616359abfd6
PRKmilo <jpizarodrugiez@gmail.com>
2023-11-16 14:22:51 -0600

    base de proyecto

---
+++ src/main/java/com/example/SistemaDistribuido/SistemaDistribuidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDistribuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDistribuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDistribuidoApplication.class, args);
+	}
+
+}
